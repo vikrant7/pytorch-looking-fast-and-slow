c594155ef30ee4badaa29cd6c31346648dac38dd
vikrant7 <vikrantnagpure721998@gmail.com>
2019-10-01 23:44:39 +0530

    Add mvod files

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# pytorch-looking-fast-and-slow
-PyTorch implementation of Looking Fast and Slow: Memory-Guided Mobile Video Object Detection 
+Mobile Video Object Detection
+========================================
+
+Code for the Paper
+
+**[Mobile Video Object Detection with Temporally-Aware Feature Maps][1]**
+Mason Liu, Menglong Zhu
+
+Implementation is in progress, all the instructions will be updated here once it is done.
+
+[1]: https://arxiv.org/abs/1711.06368
---
+++ datasets/get_VID_list.py
@@ -0,0 +1,68 @@
+import numpy as np
+import logging
+import pathlib
+import xml.etree.ElementTree as ET
+import cv2
+import os
+
+dirs = ['ILSVRC2015_VID_train_0000/',
+		'ILSVRC2015_VID_train_0001/',
+		'ILSVRC2015_VID_train_0002/',
+		'ILSVRC2015_VID_train_0003/']
+dirs_val = ['/media/sine/space/vikrant/ILSVRC2015/Data/VID/val/']
+dirs_test = ['/media/sine/space/vikrant/ILSVRC2015/Data/VID/test/']
+	
+
+
+
+file_write_obj = open('train_VID_list.txt','w')
+for dir in dirs:
+	seqs = np.sort(os.listdir(os.path.join('/media/sine/space/vikrant/ILSVRC2015/Data/VID/train/'+dir)))
+	for seq in seqs:
+		seq_path = os.path.join('/media/sine/space/vikrant/ILSVRC2015/Data/VID/train/',dir,seq)
+		relative_path = dir + seq
+		image_list = np.sort(os.listdir(seq_path))
+		for image in image_list:
+			image_id = image.split('.')[0]
+			anno_file = image_id + '.xml'
+			anno_path = os.path.join('/media/sine/space/vikrant/ILSVRC2015/Annotations/VID/train/',dir,seq,anno_file)
+			objects = ET.parse(anno_path).findall("object")
+			num_objs = len(objects)
+			if num_objs == 0:
+				continue
+			else:
+				file_write_obj.writelines(relative_path+'/'+image_id)
+				file_write_obj.write('\n')
+
+file_write_obj.close()
+file_write_obj = open('val_VID_list.txt','w')
+for dir in dirs_val:
+	seqs = np.sort(os.listdir(dir))
+	for seq in seqs:
+		seq_path = os.path.join(dir,seq)
+		image_list = np.sort(os.listdir(seq_path))
+		for image in image_list:
+			image_id = image.split('.')[0]
+			anno_file = image_id + '.xml'
+			anno_path = os.path.join('/media/sine/space/vikrant/ILSVRC2015/Annotations/VID/val/',seq,anno_file)
+			objects = ET.parse(anno_path).findall("object")
+			num_objs = len(objects)
+			if num_objs == 0:
+				continue
+			else:
+				file_write_obj.writelines(seq+'/'+image_id)
+				file_write_obj.write('\n')
+
+
+file_write_obj.close()
+file_write_obj = open('test_VID_list.txt','w')
+for dir in dirs_test:
+	seqs = np.sort(os.listdir(dir))
+	for seq in seqs:
+		seq_path = os.path.join(dir,seq)
+		image_list = np.sort(os.listdir(seq_path))
+		for image in image_list:
+			file_write_obj.writelines(seq+image)
+			file_write_obj.write('\n')
+
+file_write_obj.close()
